1afa4e611626ee105ac6b55ea76a366599ecce15
Krzysztof Kłapyta <Krystofer02@gmail.com>
2023-04-30 11:59:55 +0200

    only stdout and tessdata path set for root in jar, but it not works on linux because of using tesseract lib from system, then have to set env var

--- src/main/java/org/example/TextFromJPGReaderMain.java
+++ src/main/java/org/example/TextFromJPGReaderMain.java
@@ -4,7 +4,6 @@ import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 
 import java.io.File;
-// import java.io.FileWriter;
 import java.nio.file.FileSystems;
 import java.nio.file.Path;
 import java.nio.file.Files;
@@ -12,36 +11,30 @@ import java.nio.file.Files;
 
 public class TextFromJPGReaderMain {
 
-    private static final String TESSDATA_PATH = "";
+    private static final String TESSDATA_PATH = "./tessdata/";
     private static final String TESSERACT_LANGUAGE = "pol";
 
     public static void main(String[] args) throws TesseractException {
-		if (args.length == 0) {
-			System.out.println("You have to pass filename of picture as an positional argument.");
-			System.out.println("Example: java -jar programName img_file");
-			System.exit(-1);
-		}
-		// special path I created for pictures to process
-		// in logic: webserver put file to that folder and then program process it 
-		// XXX getDefault() in cloud solution can be a problem?
-		Path path = FileSystems.getDefault().getPath("/srv/untrusted/pictures/", args[0]);
-		if (Files.exists(path)) {
-			File imageFile = new File(path.toString());
-			Tesseract tesseract = new Tesseract();
-			tesseract.setDatapath(TESSDATA_PATH);
-			tesseract.setLanguage(TESSERACT_LANGUAGE);
-			String result = tesseract.doOCR(imageFile);
-			// collected text from file will be put in fileName_ready.txt file.
-			// to provided a asynchronous and task scheduler will check if there is a results
-			// in specified times.
-			// File readyData = new File(args[0] + "_ready.txt");
-			// FileWriter fw = new FileWriter(readyData);
-			// fw.write(result, 0, result.length());
-			System.out.println(result);
-		} else {
-			System.out.println("The file " + path.toString() + " you provided is unavailable");
-			System.exit(-1);
-		}
-		System.exit(0);
+        if (args.length == 0) {
+            System.out.println("You have to pass filename of picture as an positional argument.");
+            System.out.println("Example: java -jar programName img_file");
+            System.exit(-1);
+        }
+        // special path I created for pictures to process
+        // in logic: webserver put file to that folder and then program process it 
+        // XXX getDefault() in cloud solution can be a problem?
+        Path path = FileSystems.getDefault().getPath("/srv/untrusted/pictures/", args[0]);
+        if (Files.exists(path)) {
+            File imageFile = new File(path.toString());
+            Tesseract tesseract = new Tesseract();
+            tesseract.setDatapath(TESSDATA_PATH);
+            tesseract.setLanguage(TESSERACT_LANGUAGE);
+            String result = tesseract.doOCR(imageFile);
+            System.out.println(result);
+        } else {
+            System.out.println("The file " + path.toString() + " you provided is unavailable");
+            System.exit(-1);
+        }
+        System.exit(0);
     }
 }
