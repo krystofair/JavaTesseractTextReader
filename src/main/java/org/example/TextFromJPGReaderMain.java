2129ed27285bc82287c9817d518245e8a2372242
Krzysztof Kłapyta <Krystofer02@gmail.com>
2023-04-30 08:14:56 +0200

    like saas approach changes *img file as argument * change pom.xml to create single executable jar, but there is not loading libraries so for linux

--- src/main/java/org/example/TextFromJPGReaderMain.java
+++ src/main/java/org/example/TextFromJPGReaderMain.java
@@ -4,22 +4,44 @@ import net.sourceforge.tess4j.Tesseract;
 import net.sourceforge.tess4j.TesseractException;
 
 import java.io.File;
+// import java.io.FileWriter;
+import java.nio.file.FileSystems;
+import java.nio.file.Path;
+import java.nio.file.Files;
 
 
 public class TextFromJPGReaderMain {
 
-    private static final String FILE_EXTENSION = "jpg";
-    private static final String JPG_FILE_NAME = "kocham cie";
-    private static final String TESSDATA_PATH = "C:\\Users\\fikot\\IdeaProjects\\text-from-jpg-reader\\tessdata";
+    private static final String TESSDATA_PATH = "";
     private static final String TESSERACT_LANGUAGE = "pol";
 
     public static void main(String[] args) throws TesseractException {
-
-        File imageFile = new File("src/main/resources/" + JPG_FILE_NAME + "." + FILE_EXTENSION);
-        Tesseract tesseract = new Tesseract();
-        tesseract.setDatapath(TESSDATA_PATH);
-        tesseract.setLanguage(TESSERACT_LANGUAGE);
-        String result = tesseract.doOCR(imageFile);
-        System.out.println(result);
+		if (args.length == 0) {
+			System.out.println("You have to pass filename of picture as an positional argument.");
+			System.out.println("Example: java -jar programName img_file");
+			System.exit(-1);
+		}
+		// special path I created for pictures to process
+		// in logic: webserver put file to that folder and then program process it 
+		// XXX getDefault() in cloud solution can be a problem?
+		Path path = FileSystems.getDefault().getPath("/srv/untrusted/pictures/", args[0]);
+		if (Files.exists(path)) {
+			File imageFile = new File(path.toString());
+			Tesseract tesseract = new Tesseract();
+			tesseract.setDatapath(TESSDATA_PATH);
+			tesseract.setLanguage(TESSERACT_LANGUAGE);
+			String result = tesseract.doOCR(imageFile);
+			// collected text from file will be put in fileName_ready.txt file.
+			// to provided a asynchronous and task scheduler will check if there is a results
+			// in specified times.
+			// File readyData = new File(args[0] + "_ready.txt");
+			// FileWriter fw = new FileWriter(readyData);
+			// fw.write(result, 0, result.length());
+			System.out.println(result);
+		} else {
+			System.out.println("The file " + path.toString() + " you provided is unavailable");
+			System.exit(-1);
+		}
+		System.exit(0);
     }
 }
